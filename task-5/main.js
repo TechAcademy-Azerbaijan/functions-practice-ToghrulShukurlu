7eddda8a03d112f3915a4a81dc14a7a09293e92a
ToghrulShukurlu <106272453+ToghrulShukurlu@users.noreply.github.com>
2022-07-22 16:15:52 +0400

    Update main.js

--- task-5/main.js
+++ task-5/main.js
@@ -1,9 +1,17 @@
-const prompt = require('prompt');
-prompt.start();
+function num(a, b, c, d){
+if(a>b && a>c && a>d){
+    console.log(a);
+}
+    else if(b>a && b>c && b>d){
+        console.log(b);
+    }
+        else if(c>a && c>b && c>d){
+            console.log(c);
 
+    }
+    else{
+        console.log(d);
+    }
 
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+}
+num(1,2,3,4);
