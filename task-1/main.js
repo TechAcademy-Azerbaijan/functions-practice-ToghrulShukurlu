3005356dfcd5a4c390d43f9f17300e10efce65db
ToghrulShukurlu <106272453+ToghrulShukurlu@users.noreply.github.com>
2022-07-22 15:51:35 +0400

    Update main.js

--- task-1/main.js
+++ task-1/main.js
@@ -1,9 +1,20 @@
-const prompt = require('prompt');
-prompt.start();
-
-
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+function sum(n){
+    let totalPrice=0
+    while(n>0){
+        if(n>=100){
+            totalPrice+=100
+            n-=100
+        }
+        else if(n>=20){
+        totalPrice+=30
+        n-=20
+        }
+        else{
+            totalPrice+=n*2
+            n-=n
+        }
+    }
+    return totalPrice
+}
+let res = sum(123)
+console.log(res);
